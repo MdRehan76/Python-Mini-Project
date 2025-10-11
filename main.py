ea3d53c3c658091c453b244adb2ee1e35bab2e76
MdRehan76 <mohd.rehanmalim@gmail.com>
2025-10-11 22:18:11 +0530

    Welcome page is created

--- main.py
+++ main.py
@@ -5,16 +5,45 @@ import pygame
 from pygame.locals import * # Basic pygame imports
 
 # Globals Variables for the game
-FPS = 50
-ScreenWidth = 289
-ScreenHeight = 511
+FPS = 32
+ScreenWidth = 360
+ScreenHeight = 640
 Screen = pygame.display.set_mode((ScreenWidth,ScreenHeight))
-GroundY = ScreenHeight * 0.8
+GroundY = ScreenHeight * 0.8 #For Base
 Game_Photos = {}
 Game_Sound = {}
-Player = 'Gallery/Photos/Bird.jpg'
+Player = 'Gallery/Photos/Bird.png'
 Background = 'Gallery/Photos/Background.jpg'
 Pipe = 'Gallery/Photos/pipe.png'
+
+def welcomeScreen():
+    """
+    Shows Welcome Images on the screen 
+    """ 
+    messagex = int((ScreenHeight - Game_Photos['message'].get_width())/16)
+    messagey = int(ScreenHeight*0.13)
+    PlayerX = int(ScreenWidth/7)
+    PlayerY = int((ScreenHeight - Game_Photos['Player'].get_height())/2)
+    basex = 0
+
+    while True:
+        for event in pygame.event.get():
+            #if user clicks on cross button, close the game
+            if event.type == QUIT or (event.type == KEYDOWN and event.type == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                return 
+            else:
+                Screen.blit(Game_Photos['Background'],(0,0)) 
+                Screen.blit(Game_Photos['Player'],(PlayerX,PlayerY)) 
+                Screen.blit(Game_Photos['message'],(messagex,messagey)) 
+                Screen.blit(Game_Photos['Base'],(basex,GroundY )) 
+                pygame.display.update()
+                FPSCLOCK.tick(FPS) # To Control the game FPS
+
+
 
 if __name__ == "__main__":
     #This will be main point from where our game will start
@@ -35,13 +64,22 @@ if __name__ == "__main__":
     )
 
     Game_Photos['message'] = pygame.image.load('Gallery/Photos/message.png').convert_alpha()
-    Game_Photos['Base'] = pygame.image.load('Gallery/Photos/Base.jpg').convert_alpha()
+    Game_Photos['Base'] = pygame.image.load('Gallery/Photos/Base.png').convert_alpha()
     Game_Photos['Pipe'] = (
         pygame.transform.rotate(pygame.image.load( Pipe).convert_alpha(), 180),
         pygame.image.load( Pipe).convert_alpha()
         )
-    Game_Sound['Die'] = pygame.mixer.sound('Gallery/Sound/Die.mp3')
-    Game_Sound['Hit'] = pygame.mixer.sound('Gallery/Sound/Hit.mp3')
-    Game_Sound['Point'] = pygame.mixer.sound('Gallery/Sound/Point.mp3')
-    Game_Sound['Swoosh'] = pygame.mixer.sound('Gallery/Sound/Swoosh.mp3')
-    Game_Sound['Wing'] = pygame.mixer.sound('Gallery/Sound/Wing.mp3')
+    Game_Sound['Die'] = pygame.mixer.Sound('Gallery/Sound/Die.mp3')
+    Game_Sound['Hit'] = pygame.mixer.Sound('Gallery/Sound/Hit.mp3')
+    Game_Sound['Point'] = pygame.mixer.Sound('Gallery/Sound/Point.mp3')
+    Game_Sound['Swoosh'] = pygame.mixer.Sound('Gallery/Sound/Swoosh.mp3')
+    Game_Sound['Wing'] = pygame.mixer.Sound('Gallery/Sound/Wing.mp3')
+
+        
+    Game_Photos['Background'] = pygame.image.load(Background).convert()
+    Game_Photos['Player'] = pygame.image.load(Player).convert_alpha()
+
+    while True: 
+        welcomeScreen() #Shows welcome Screen to the user until he presses a button
+        mainGame() #This is the main Game Function 
+        
